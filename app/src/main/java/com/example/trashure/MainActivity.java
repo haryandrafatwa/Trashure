0b262575065552e233a3b87bd920012539d0de6e
hamadfauzi <fauzihamad37@gmail.com>
2019-06-07 21:58:53 +0700

    membuat bottomNavBar dan Fragment kelas penukaran,beranda,harga,scan,akun

--- app/src/main/java/com/example/trashure/MainActivity.java
+++ app/src/main/java/com/example/trashure/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.trashure;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavBar);
+        final AkunFragment akunFragment = new AkunFragment();
+        final PenukaranFragment penukaranFragment = new PenukaranFragment();
+        final ScanFragment scanFragment = new ScanFragment();
+        final BerandaFragment berandaFragment = new BerandaFragment();
+        final HargaFragment hargaFragment = new HargaFragment();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+            {
+
+                int id = menuItem.getItemId();
+                if (id == R.id.menuAkun) {
+                    setFragment(akunFragment);
+                    return true;
+                } else if (id == R.id.menuBeranda) {
+                    setFragment(berandaFragment);
+                    return true;
+                } else if (id == R.id.menuHarga) {
+                    setFragment(hargaFragment);
+                    return true;
+                } else if (id == R.id.menuPenukaran) {
+                    setFragment(penukaranFragment);
+                    return true;
+                } else if (id == R.id.menuScan) {
+                    setFragment(scanFragment);
+                    return true;
+                }
+                return  false;
+            }
+
+        });
+        bottomNavigationView.setSelectedItemId(R.id.menuBeranda);
+
+    }
+    public void setFragment(Fragment fragment)
+    {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameFragment,fragment);
+        fragmentTransaction.commit();
     }
 }
