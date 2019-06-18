6ea4439348cda2dfbcff6c8920fcd4637b36a900
hamadfauzi <fauzihamad37@gmail.com>
2019-06-19 00:40:03 +0700

    membuat tampilan penukaran

--- app/src/main/java/com/example/trashure/PenukaranFragment.java
+++ app/src/main/java/com/example/trashure/PenukaranFragment.java
@@ -7,7 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
 
 
 public class PenukaranFragment extends Fragment {
@@ -16,6 +19,34 @@ public class PenukaranFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        eventPenukaran();
+    }
+
+    private void eventPenukaran() {
+        final ExpandableRelativeLayout expandableRelativeLayout = (ExpandableRelativeLayout) getActivity().findViewById(R.id.expandableLayout);
+        final LinearLayout llPulsa = (LinearLayout) getActivity().findViewById(R.id.ll_pulsa);
+        final ImageView arrow = (ImageView) getActivity().findViewById(R.id.iv_penukaran_pulsa);
+        expandableRelativeLayout.toggle();
+        llPulsa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(expandableRelativeLayout.isExpanded())
+                {
+                    expandableRelativeLayout.toggle();
+                    arrow.setImageResource(R.drawable.ic_arrow_drop_down);
+                }
+                else
+                {
+                    expandableRelativeLayout.toggle();
+                    arrow.setImageResource(R.drawable.ic_arrow_drop_up);
+                }
+            }
+        });
     }
 
     @Override
