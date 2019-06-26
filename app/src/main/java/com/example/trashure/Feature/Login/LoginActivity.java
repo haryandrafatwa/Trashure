d024ba16b7a16427b1873c44100ab9d9f52f3c09
Hamad Fauzi Jessar <hamadfauzi23@gmail.com>
2019-06-26 11:16:03 +0700

    add logout

--- app/src/main/java/com/example/trashure/Feature/Login/LoginActivity.java
+++ app/src/main/java/com/example/trashure/Feature/Login/LoginActivity.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -16,11 +19,19 @@ import android.widget.Toast;
 import com.example.trashure.Feature.Register.RegisterActivity;
 import com.example.trashure.MainActivity;
 import com.example.trashure.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -31,13 +42,15 @@ public class LoginActivity extends AppCompatActivity {
     EditText etEmail,etPassword;
     CheckBox cbIngatSaya;
     TextView tvLupaPassword,tvDaftar;
-
+    private String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initialize();
+
+
 
     }
 
@@ -53,6 +66,30 @@ public class LoginActivity extends AppCompatActivity {
         cbIngatSaya = (CheckBox) findViewById(R.id.cb_ingat_saya);
         tvDaftar = (TextView) findViewById(R.id.tv_daftar);
         tvLupaPassword = (TextView) findViewById(R.id.tv_lupa_password);
+
+        etEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!etEmail.getText().toString().matches(emailPattern))
+                {
+                    etEmail.setTextColor(getResources().getColor(R.color.red));
+                }
+                else{
+                    etEmail.setTextColor(getResources().getColor(R.color.colorInputText));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
 
         tvDaftar.setOnClickListener(new View.OnClickListener()
         {
@@ -84,6 +121,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
+
     private void sendUserToLoginActivity()
     {
         Intent homeIntent = new Intent(LoginActivity.this,LoginActivity.class);
@@ -93,6 +131,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private void LoginWithGoogle()
     {
+
     }
 
     private void LoginWithFacebook()
